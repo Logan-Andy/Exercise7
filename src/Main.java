90fa56f69b4da8c728a1bb941c0e3ee629998c08
Logan J Andy 12th WHS <148928@SWCSD.US>
2025-02-25 13:42:09 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        ContactInfo contactInfo1 = new ContactInfo("Logan", "awesomejandy@gmail.com");
+        contactInfo1.display();
+        BusinessInfo businessInfo1 = new BusinessInfo("Omar", "116234@swcsd.us","6145800000");
+        businessInfo1.display();
+    }
+}
